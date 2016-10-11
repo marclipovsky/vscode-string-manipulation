34e135d17073cb362d62d59b953685ff4e5a8f76
Marc Lipovsky <marc@nestlabs.com>
2016-10-11 16:49:25 -0700

    Init extension commit

---
+++ extension.js
@@ -0,0 +1,42 @@
+const vscode = require('vscode');
+const string = require('underscore.string');
+const editor = vscode.window.activeTextEditor;
+const commands = [
+  "titlize",
+  "camelize",
+  "classify",
+  "underscored",
+  "dasherize",
+  "humanize",
+  "slugify",
+  "reverse",
+  "swapCase",
+  "join",
+  "decapitalize",
+  "capitalize"
+];
+
+function holder(commandName) {
+  if (!string[commandName]) { return; }
+
+  if (!editor) { return; }
+
+  const selection = editor.selection;
+  let text = editor.document.getText(selection);
+
+  if (commandName === 'join') {
+    replaced = string[commandName](" ", ...text.split("\n"));
+  } else {
+    replaced = string[commandName](text);
+  }
+
+  editor.edit(builder => builder.replace(selection, replaced));
+}
+
+exports.activate = function activate(context) {
+  commands.forEach(commandName => {
+    context.subscriptions.push(vscode.commands.registerCommand(`string-manipulation.${commandName}`, _ => holder(commandName)));
+  })
+}
+
+exports.deactivate = function deactivate() { }
