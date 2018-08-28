bf25cd03d1e261b410e542bdd5acf63718abbd22
Marc Lipovsky <marclipovsky@Marcs-Mac-mini.attlocal.net>
2018-08-27 21:19:20 -0700

    Add slugify and fix #6,#7

--- extension.js
+++ extension.js
@@ -1,11 +1,13 @@
+
 const {
   window: { activeTextEditor: editor },
-  commands: { registerCommand }
+  commands
 } = require('vscode');
 const _string = require('underscore.string');
 const apStyleTitleCase = require('ap-style-title-case');
 const chicagoStyleTitleCase = require('chicago-capitalize');
-const commands = [
+const slugify = require('@sindresorhus/slugify');
+const commandNames = [
   "titleize",
   "camelize",
   "classify",
@@ -23,7 +25,7 @@ const commands = [
   "titleize-chicago-style",
 ];
 
-const holder = commandName => {
+const stringFunction = commandName => {
   if (!editor) { return; }
 
   editor.edit(builder => {
@@ -34,13 +36,17 @@ const holder = commandName => {
       const text = editor.document.getText(selection);
       const textParts = text.split('\n');
 
-      switch (commandName) {
-        case 'join':
+      switch (true) {
+        case commandName.match('join'):
           replaced = _string[commandName](" ", ...textParts);
           builder.replace(selection, replaced);
           return;
 
-        case 'screaming-snake':
+        case commandName.match('camelize'):
+          stringFunc = str => _string.camelize(str.toLowerCase());
+          break;
+
+        case commandName.match('screaming-snake'):
           stringFunc = str => _string.underscored(str)
             .replace(/([A-Z])[^A-Z]/g, " $1")
             .replace(/[^a-z]+/ig, " ")
@@ -49,16 +55,24 @@ const holder = commandName => {
             .toUpperCase();
           break;
 
-        case 'titleize-ap-style':
+        case commandName.match('slugify'):
+          stringFunc = slugify;
+          break;
+
+        case commandName.match(/case$/):
+          stringFunc = _string[commandName];
+          break;
+
+        case commandName.match('titleize-ap-style'):
           stringFunc = apStyleTitleCase;
           break;
 
-        case 'titleize-chicago-style':
+        case commandName.match('titleize-chicago-style'):
           stringFunc = chicagoStyleTitleCase;
           break;
 
         default:
-          stringFunc = _string[commandName];
+          stringFunc = (str) => _string[commandName](str.toLowerCase());
           break;
       }
 
@@ -69,9 +83,7 @@ const holder = commandName => {
 }
 
 exports.activate = context => {
-  commands.forEach(commandName => {
-    context.subscriptions.push(registerCommand(`string-manipulation.${commandName}`, () => holder(commandName)));
-  })
+  commandNames.forEach(commandName => {
+    context.subscriptions.push(commands.registerCommand(`string-manipulation.${commandName}`, () => stringFunction(commandName)));
+  });
 }
-
-exports.deactivate = () => { };
