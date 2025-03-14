1f2be4fa1b5d48aab887673870b282e3c1508407
Marc Lipovsky <marclipovsky@gmail.com>
2025-03-14 13:29:50 -0400

    Add string transformation preview functionality to context menu

--- src/extension.ts
+++ src/extension.ts
@@ -1,9 +1,13 @@
 import * as vscode from "vscode";
 import { StringManipulationSidebar } from "./sidebar";
 import { activate as stringManipulationActivate } from "./commands/index";
+import { registerPreviewCommand } from "./commands/preview";
 
 export function activate(context: vscode.ExtensionContext) {
   stringManipulationActivate(context);
+
+  // Register command to show transformations with previews
+  registerPreviewCommand(context);
 
   const sidebarProvider = new StringManipulationSidebar(context);
 
